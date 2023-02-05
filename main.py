250413e63d08c9698327a6a72ab1534cb6d0f9dd
Sam Alex Koshy <samuel.alex.koshy@gmail.com>
2023-02-05 18:57:11 +0530

    created function to store file data
    The program can now store the data of how many files have the year and type and saves accordingly.

--- main.py
+++ main.py
@@ -106,11 +106,15 @@ class DriveReader():
 
                 else:
                     if main_folder_id is not None:
+                        with open("database.json", "w") as file:
+                            json_obj = json.dumps({})
+                            file.write(json_obj)
                         files = self.search_folders(main_folder_id, 
                                                     "Project DriveReader")
                         with open("data.json", "w") as f:
                             json_obj = json.dumps(files, indent=4)
                             f.write(json_obj)
+                        time.sleep(10)
                     else:
                         print("Could not find the folder.")
 
@@ -141,6 +145,7 @@ class DriveReader():
                                                               file.get("name"))
                         files.append(returned_folder)
                     else:
+                        self.categorize_data(parent_name, file.get("name"))
                         files.append((file.get("name")))
                 page_token = response.get("nextPageToken", None)
                 if page_token is None:
@@ -151,6 +156,37 @@ class DriveReader():
 
         return {parent_name:files}
 
+    def categorize_data(self, teacher_name:str, file_name:str):
+        """Categorize the data from files to a database."""
+        name_data = file_name.split("_", 2)
+        if len(name_data) < 3:
+            return
+        year, type, name = name_data
+        with open("database.json", "r") as file:
+            try:
+                data = json.load(file)
+            except json.decoder.JSONDecodeError:
+                data = {}
+        teacher_data = data.get(teacher_name)
+        if teacher_data is None:
+            data.update({teacher_name:{year:{type:1}}})
+        else:
+            if teacher_data.get(year) is not None:
+                year_data = teacher_data[year]
+                if year_data.get(type) is not None:
+                    count = year_data[type]
+                    year_data.update({type:count+1})
+                else:
+                    year_data.update({type:1})
+                teacher_data.update({year:year_data})
+            else:
+                teacher_data.update({year:{type:1}})
+            data.update({teacher_name:teacher_data})
+        print(data)
+        with open("database.json", "w+") as file:
+            json_send_obj = json.dumps(data, indent=4)
+            file.write(json_send_obj)
+
     def main(self):
         """The main function of the class."""
         try:
@@ -160,10 +196,12 @@ class DriveReader():
                     self.search_for_all_folders()
                 elif command == "quit" or command == "exit":
                     sys.exit()
-                elif command == "search":
+                elif command == "search" or command == "run":
                     self.categorize_folders_from_drive()
-                elif command == "run":
-                    time.sleep(100)
+                # elif command == "run":
+                #     time.sleep(100)
+                elif command == "data":
+                    self.categorize_data("")
                 else:
                     print("Invalid Command")
         except KeyboardInterrupt:
