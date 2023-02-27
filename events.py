d067c1f651e99760d7aadeac4ba0af510f2459f6
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2023-02-27 23:02:11 +0530

    added doc string for module

--- events.py
+++ events.py
@@ -1,3 +1,14 @@
+"""
+This project intends to read the required files from Google Drive, and 
+then based on the naming convention applied to those files, analyse and 
+categorize the available data into the necessary format. 
+
+Project Creation: Feb 3, 2023
+Lead Developer: Sam Alex Koshy
+Project Contributors: Ashok Immanuel, Rohini V.
+"""
+
+
 from __future__ import print_function
 
 import io
