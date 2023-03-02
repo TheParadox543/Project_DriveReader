ae0640cc5c7694e5407670d6805148ef93a023ee
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2023-03-02 10:50:40 +0530

    comments, logging
    Added logging module to track the changes.
    Added more comments to make the code more readable.

--- project_drivereader/main.py
+++ project_drivereader/main.py
@@ -12,12 +12,15 @@ Project Contributors: Ashok Immanuel, Rohini V.
 # Install necessary libraries with pip install -r requirements.txt
 from __future__ import print_function
 
+# Import in-built modules.
+import logging
 from io import BytesIO
 from json import dumps, load
 from os import path, system as ossystem
 from pprint import PrettyPrinter
 from sys import exit as sysexit
 
+# Import project specific modules.
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -26,7 +29,7 @@ from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaIoBaseDownload
 from openpyxl import load_workbook
 from openpyxl.styles import Alignment, Font
-from openpyxl.utils import get_column_letter
+from openpyxl.utils import get_column_letter as get_col_let
 from openpyxl.workbook.workbook import Workbook
 from openpyxl.worksheet.worksheet import Worksheet
 
@@ -37,7 +40,16 @@ SCOPES = [
 
 pp = PrettyPrinter(indent=4)
 
+# Using the logs.
+logger_monitor = logging.getLogger(__name__)
+logger_monitor.setLevel(logging.DEBUG)
+handler = logging.FileHandler("drive_reader_logs.log")
+handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
+logger_monitor.addHandler(handler)
 
+
+# * A few classes to identify the different data extracted from drive
+# * and excel.
 class Category():
     """"""
 
@@ -54,6 +66,22 @@ class Year():
     """"""
 
 
+def sort_dictionary(unsorted_dict: dict[str, ], reverse=False):
+    """A util function to sort the keys of a dictionary.
+    
+    Parameters
+    ---------
+    - unsorted_dict`dict[str, Any]`: The dictionary that needs to be sorted.
+    - reverse`bool`: Whether the keys need to be sorted in reverse order.
+
+    Returns
+    ------
+    - sorted_dict`dict[str, Any]`: The dictionary with keys sorted.
+    """
+    order_list: list[str] = sorted(unsorted_dict.keys(), reverse=reverse)
+    sorted_dictionary = dict({i: unsorted_dict[i] for i in order_list})
+    return sorted_dictionary
+
 class ExcelWorker():
     """The class that will handle interaction with the excel workbooks."""
 
@@ -66,8 +94,10 @@ class ExcelWorker():
         # Load the workbook, if not exit the program.
         try:
             self.doc_wb:Workbook = load_workbook("data/doc_classification.xlsx")
+            logger_monitor.debug("Classification file found.")
         except FileNotFoundError: 
             print("Classification file not found.")
+            logger_monitor.warning("Classification file not found.")
             sysexit()
 
         # * The following are the data to be extracted from the excel sheet.
@@ -101,18 +131,35 @@ class ExcelWorker():
                     if classification not in self.classification_list:
                         self.classification_list[classification] = category
 
+            #Any other sheet, check if any code is missing and then add it. 
             else:
                 for row in ws.iter_rows(min_col=2, max_col=3):
                     code, name = row[0].value, row[1].value
                     if code and name and code not in self.code_list:
                         self.code_list[code] = [name, category, ["Unknown"]]
+
+        #Write the generated data to files for evaluation.
         with open("data/code_list.json", "w") as file:
             code_obj = dumps(self.code_list, indent=4)
             file.write(code_obj)
+        with open("data/classification_list.json", "w") as file:
+            class_obj = dumps(self.classification_list, indent=4)
+            file.write(class_obj)
+        logger_monitor.debug(self.code_list)
+        logger_monitor.debug(self.classification_list)
 
-    def write_data_to_excel(self, drive_data: dict[Category, dict[Year, dict[Code, int]]], 
-                       exempted: list[tuple[Name, str]]):
-        """Write data from the drive to the excel sheet."""
+    def write_data_to_excel(self, 
+            drive_data: dict[Category, dict[Year, dict[Code, int]]], 
+            exempted: list[tuple[Name, str]]):
+        """Write data from the drive to the excel sheet.
+
+        Parameters
+        ----------
+        - drive_data: The raw data of the different files that satisfy the 
+        necessary conditions in all folders.  
+        - exempted: The files that are exempted from classification due to any
+        reason.
+        """
         workbook = Workbook()
         workbook.active.title = "exempted"
 
@@ -121,17 +168,19 @@ class ExcelWorker():
             worksheet: Worksheet = workbook.create_sheet(category, -1)
             category_data = drive_data[category]
             worksheet.append(["YEAR", "CLASSIFICATION", "COUNT"])
+            # Format headers in each sheet.
             for i in range(1, 4):
-                worksheet[f"{get_column_letter(i)}1"].alignment = Alignment(horizontal="center")
-                worksheet[f"{get_column_letter(i)}1"].font = Font(bold=True, size=12)
+                worksheet[f"{get_col_let(i)}1"].alignment = Alignment(horizontal="center")
+                worksheet[f"{get_col_let(i)}1"].font = Font(bold=True, size=12)
+
+            # Append data to the sheet.
             start, stop, width = 2, 2, 16
-            for year in category_data:
+            for year, year_data in category_data.items():
                 worksheet[f"A{start}"] = year
-                year_data = category_data[year]
-                for code in year_data:
+                for code, val in year_data.items():
                     name = self.code_list[code][0]
                     worksheet[f"B{stop}"] = name
-                    worksheet[f"C{stop}"] = year_data[code]
+                    worksheet[f"C{stop}"] = val
                     width = max(width, len(name))
                     stop += 1
 
@@ -140,26 +189,28 @@ class ExcelWorker():
                 worksheet[f"A{start}"].alignment = Alignment(horizontal="center", 
                                                              vertical="center")
                 start = stop
+            # Fix width to readable length.
             worksheet.column_dimensions["B"].width = width
             worksheet.column_dimensions["A"].width = 13
 
+        # Handle exempted data.
         worksheet = workbook["exempted"]
         worksheet.append(["File Name", "Folder name"])
+        for i in range(1, 3):
+            worksheet[f"{get_col_let(i)}1"].alignment = Alignment(horizontal="center")
+            worksheet[f"{get_col_let(i)}1"].font = Font(bold=True, size=12)
         width1, width2 = 13, 13
+        # Append data to the sheet.
         for value in exempted:
             worksheet.append(value)
             width1, width2 = max(width1, len(value[0])), max(width2, len(value[1]))
+        # Fix width to readable length
         worksheet.column_dimensions["A"].width = width1
         worksheet.column_dimensions["B"].width = width2
-        for i in range(1, 3):
-            worksheet[f"{get_column_letter(i)}1"].alignment = Alignment(horizontal="center")
-            worksheet[f"{get_column_letter(i)}1"].font = Font(bold=True, size=12)
+
+        # Save the workbook, close any instance if saving fails.
         while True:
             try:
-                # try:
-                #     os.system("taskkill/im EXCEL.EXE categorized.xlsx")
-                # except:
-                #     pass
                 workbook.save("data/categorized.xlsx")
             except PermissionError:
                 try:
@@ -168,12 +219,20 @@ class ExcelWorker():
                     pass
             else:
                 break
+        # * Open the workbook to see the result.
         # os.system("start EXCEL.EXE categorized.xlsx")
 
     def write_naac_data_to_excel(self, 
-                                 drive_data: dict[Category, dict[Year, dict[Code, int]]]):
-        """Write data to excel sheet in naac required format."""
-        spec_data = {}
+            drive_data: dict[Category, dict[Year, dict[Code, int]]]):
+        """Write data to excel sheet in naac required format.
+
+        Parameters
+        ----------
+        - drive_data: The raw data of the different files that satisfy the 
+        necessary conditions in all folders."""
+        spec_data = {} # Alias for classification data.
+
+        # Take the data for 22-23 and put in naac excel.
         for category_data in drive_data.values():
             for year, year_data in category_data.items():
                 if year == "2022-2023":
@@ -181,14 +240,12 @@ class ExcelWorker():
                         if self.code_list.get(code):
                             for spec in self.code_list[code][2]:
                                 spec_data.update({spec: spec_data.get(spec, 0) + value})
-        order_list = sorted(spec_data.keys())
-        spec_data = dict([(i, spec_data[i]) for i in order_list])
+        spec_data = sort_dictionary(spec_data)
+
         naac_wb = Workbook()
         naac_ws: Worksheet = naac_wb.active
-        start, stop = 1, 1
-        old_number = 0
-        width = 13
-        # print(spec_data)
+        start, width = 1, 13
+        logger_monitor.debug(spec_data)
     
         # * Entering the data that is there.
         # for spec in spec_data:
@@ -211,23 +268,34 @@ class ExcelWorker():
         naac_ws.append(["Classification", "Code", "Count"])
         start, word = 1, None
         for num, (classification, category) in enumerate(self.classification_list.items(), 2):
-            naac_ws.append([category, classification, spec_data.get(classification, 0)])
+            naac_ws.append({
+                    "B": classification, 
+                    "C": spec_data.get(classification, 0)
+            })
             width = max(width, len(category)*1.2)
+            # When word changes, merge cells.
             if word != category:
                 naac_ws.merge_cells(f"A{start}:A{num-1}")
                 naac_ws[f"A{start}"].alignment = Alignment(horizontal="center", 
                                                            vertical="center")
+                naac_ws[f"A{start}"] = category
                 start, word = num, category
         else:
+            # Merge remaining categories.
             naac_ws.merge_cells(f"A{start}:A{num}")
             naac_ws[f"A{start}"].alignment = Alignment(horizontal="center", 
                                                         vertical="center")
+            naac_ws[f"A{start}"] = category
+
+            # Fix alignment and font for headers.
             for i in range(1, 4):
-                naac_ws[f"{get_column_letter(i)}1"].alignment = Alignment(
+                naac_ws[f"{get_col_let(i)}1"].alignment = Alignment(
                     horizontal="center", vertical="center")
-                naac_ws[f"{get_column_letter(i)}1"].font = Font(bold=True, size=12)
+                naac_ws[f"{get_col_let(i)}1"].font = Font(bold=True, size=12)
+            # Improve readability of category column.
             naac_ws.column_dimensions["A"].width = width
 
+        # Save the workbook, close any instances if saving fails.
         while True:
             try:
                 naac_wb.save("data/naac.xlsx")
@@ -368,8 +436,8 @@ class DriveReader():
         else:
             for category in list(self.data.keys()):
                 for year, year_data in self.data[category].items():
-                    self.data[category][year] = self.sort_dictionary(year_data)
-                self.data[category] = self.sort_dictionary(self.data[category], True)
+                    self.data[category][year] = sort_dictionary(year_data)
+                self.data[category] = sort_dictionary(self.data[category], True)
                 if "0" in self.data[category]:
                     self.data.pop(category)
 
@@ -408,12 +476,6 @@ class DriveReader():
                 self.data.update({category: category_data})
                 if "0" in self.data:
                     self.data.pop("0")
-
-    def sort_dictionary(self, unsorted_dict: dict[str, ], reverse=False):
-        """A util function to sort the keys of a dictionary."""
-        order_list: list[str] = sorted(unsorted_dict.keys(), reverse=reverse)
-        sorted_dictionary = dict({i: unsorted_dict[i] for i in order_list})
-        return sorted_dictionary
 
     def main(self):
         """The main function of DriveReader class."""
