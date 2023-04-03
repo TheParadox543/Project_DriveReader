c40b7031fd2162afc77e01702aac9be25ec5363c
Sam Alex Koshy <samuel.alex.koshy@gmail.com>
2023-04-03 14:22:59 +0530

    docs: add docstring for __init__

--- src/drivereader/__init__.py
+++ src/drivereader/__init__.py
@@ -1,3 +1,15 @@
+"""
+Drive API Wrapper
+~~~~~~~~
+This project intends to read the required files from Google Drive, and
+then based on the naming convention applied to those files, analyse and
+categorize the available data into the necessary format.
+
+Project Creation: Feb 3, 2023
+Lead Developer: Sam Alex Koshy
+Project Contributors: Ashok Immanuel, Rohini V.
+"""
+
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("drivereader")
